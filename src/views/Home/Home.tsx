9251b005e16392b7220724d8cac6d4c2bdb2725e
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 15:44:29 -0400

    Add PlantSwap Development Fund Description to Home page

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -76,6 +76,17 @@ const Home: React.FC = () => {
         </Cards>
       </div>
       <Divider />
+      <Heading as="h2" size="xl" mb="14px">🌲The PlantSwap Development Fund will help plant trees and other environmental causes</Heading>
+          <br />
+          <Text>Some pools will have deposit fees as well as some of the transaction with the Master Gardener smart contract result in PLANT been send to the Development Fund address. What will we do with the fund?</Text>
+          <br />
+          <Text>45%🌲 will go to plant trees 🌲</Text>
+          <Text>45%🔥 will be burn to lower the total supply 🔥</Text>
+          <Text>10%💸 keep in treasury to cover operating expanse 💸</Text>
+          <br />
+          <Text>A governance token to chose the right organization and causes to support</Text>
+          <Text>Later this year, when the community around Plant Swap finance will have grown, we will release a governance token to decentralize the decision making on which causes to support, what goal to focus on and regulate the different economic incentives of this eco-system. More on this later…</Text>
+        <br /><br />
         <StyledImage src="/images/endPage.svg" alt="PlantSwap Finance" width={680} height={155} />
     </Page>
   )
