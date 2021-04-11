8e52c014deacffdf0ce27e567d6a634c30b607b9
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 14:04:31 -0400

    Add PlantToken and MasterGardener ABI + MasterGardener Tesnet Addess

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,6 +1,6 @@
 export default {
   masterChef: {
-    97: '0x1d32c2945C8FDCBc7156c553B7cEa4325a17f4f9',
+    97: '0xd6756c4876ACD3c0162AF74Ba25b6b78F951836b',
     56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
   },
   sousChef: {
