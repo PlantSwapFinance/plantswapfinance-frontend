1f8d84fe3381d46f3760eadf38f15a68b916f61c
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-11 14:01:06 -0400

    Update Plant Garden Master Garden Address

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -7,27 +7,13 @@ const pools: PoolConfig[] = [
     stakingToken: tokens.plant,
     earningToken: tokens.plant,
     contractAddress: {
-      97: '0xD353738494c18d779E8279507032493e60603199',
-      56: '0x35418e14F5aA615C4f020eFBa6e01C5DbF15AdD2',
+      56: '0x58BA5Bd8872ec18BD360a9592149daed2fC57c69',
+      97: '0xd6756c4876ACD3c0162AF74Ba25b6b78F951836b',
     },
     poolCategory: PoolCategory.CORE,
     harvest: true,
     sortOrder: 1,
-    tokenPerBlock: '2.3148',
-  },
-  {
-    sousId: 0,
-    stakingToken: tokens.cake,
-    earningToken: tokens.cake,
-    contractAddress: {
-      97: '0xd3af5fe61dbaf8f73149bfcfa9fb653ff096029a',
-      56: '0x73feaa1eE314F8c655E354234017bE2193C9E24E',
-    },
-    poolCategory: PoolCategory.CORE,
-    harvest: true,
-    tokenPerBlock: '10',
-    sortOrder: 1,
-    isFinished: false,
+    tokenPerBlock: '0.01',
   },
 ]
 
