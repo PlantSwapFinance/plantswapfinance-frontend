30b45664c3fd29cb79246a37e2b29fe1815cd684
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-21 12:17:45 -0400

    Fix call function name

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingCake',
+        name: 'pendingPlant',
         params: [farm.pid, account],
       }))
 
