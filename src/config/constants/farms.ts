fd6d7e5131a502a272838519954e6ca6ae581a81
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-21 08:41:28 -0400

    Add farms address

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,34 +4,24 @@ import { FarmConfig } from './types'
 const farms: FarmConfig[] = [
 
   {
-    pid: 3,
+    pid: 1,
     lpSymbol: 'PLANT-BNB LP',
     lpAddresses: {
-      97: '0xcba2850439ccb4228906b886d45f96063b7e26f3',
-      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      97: '',
+      56: '0xd0dc9cc35ed1584f4858e5194bd9b153b70b8a83',
     },
     token: tokens.plant,
     quoteToken: tokens.wbnb,
   },
   {
-    pid: 4,
+    pid: 3,
     lpSymbol: 'PLANT-BUSD LP',
     lpAddresses: {
-      97: '0x2f7682b64b88149ba3250aee32db712964de5fa9',
-      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
+      97: '',
+      56: '0xf71070df7c352a2668266703dc5d3d539d336bb0',
     },
     token: tokens.plant,
     quoteToken: tokens.busd,
-  },
-  {
-    pid: 5,
-    lpSymbol: 'PLANT-DAI LP',
-    lpAddresses: {
-      97: '0x2f7682b64b88149ba3250aee32db712964de5fa9',
-      56: '0x1b96b92314c44b159149f7e0303511fb2fc4774f',
-    },
-    token: tokens.plant,
-    quoteToken: tokens.dai,
   },
   {
     pid: 2,
@@ -42,16 +32,7 @@ const farms: FarmConfig[] = [
     },
     token: tokens.busd,
     quoteToken: tokens.wbnb,
-  },
-  {
-    pid: 1,
-    lpSymbol: 'CAKE-BNB LP',
-    lpAddresses: {
-      97: '0xe70b7523f4bffa1f2e88d2ba709afd026030f412',
-      56: '0xA527a61703D82139F8a06Bc30097cC9CAA2df5A6',
-    },
-    token: tokens.cake,
-    quoteToken: tokens.wbnb,
+    isCommunity: true,
   },
 ]
 
