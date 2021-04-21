31ced145132baadd985701ce659e56b4c7b5b4ae
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-04-21 10:17:02 -0400

    quick fix

--- src/hooks/useApprove.ts
+++ src/hooks/useApprove.ts
@@ -3,7 +3,7 @@ import { useWeb3React } from '@web3-react/core'
 import { Contract } from 'web3-eth-contract'
 import { ethers } from 'ethers'
 import { useDispatch } from 'react-redux'
-import { updateUserAllowance, fetchFarmUserDataAsync } from 'state/actions'
+import { fetchFarmUserDataAsync } from 'state/actions'
 import { approve } from 'utils/callHelpers'
 import { useMasterchef, useCake, useLottery } from './useContract'
 
@@ -27,7 +27,7 @@ export const useApprove = (lpContract: Contract) => {
 }
 
 // Approve a Pool
-export const useSousApprove = (lpContract: Contract, sousId) => {
+export const useSousApprove = (lpContract: Contract) => {
   const dispatch = useDispatch()
   const { account } = useWeb3React()
   const masterChefContract = useMasterchef()
