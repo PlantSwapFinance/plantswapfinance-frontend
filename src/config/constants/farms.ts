600c2456d3ef07d2ae7d2f247438bf78cb574859
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-24 07:39:41 -0400

    Add farm with V2 LP's

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -3,6 +3,28 @@ import { FarmConfig } from './types'
 
 const farms: FarmConfig[] = [
 
+  // New V2
+  {
+    pid: 4,
+    lpSymbol: 'PLANT-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0x79268898de0f8c67ad73d9e33534d9874411aaaa',
+    },
+    token: tokens.plant,
+    quoteToken: tokens.wbnb,
+  },
+  {
+    pid: 5,
+    lpSymbol: 'PLANT-BUSD LP',
+    lpAddresses: {
+      97: '',
+      56: '0xf71070df7c352a2668266703dc5d3d539d336bb0',
+    },
+    token: tokens.plant,
+    quoteToken: tokens.busd,
+  },
+  // V1 (Not to use anymore)
   {
     pid: 1,
     lpSymbol: 'PLANT-BNB LP',
