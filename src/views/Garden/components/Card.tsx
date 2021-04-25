315717485fb7063c1c4ee0c7e9466477c55a063c
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-25 17:07:13 -0400

    Change active card color

--- src/views/Garden/components/Card.tsx
+++ src/views/Garden/components/Card.tsx
@@ -8,7 +8,7 @@ const Card = styled.div<{ isActive?: boolean; isFinished?: boolean }>`
   color: ${({ isFinished, theme }) => theme.colors[isFinished ? 'textDisabled' : 'secondary']};
   box-shadow: ${({ isActive }) =>
     isActive
-      ? '0px 0px 0px 1px #0098A1, 0px 0px 4px 8px rgba(31, 199, 212, 0.4);'
+      ? '0px 0px 0px 1px #399349, 0px 0px 4px 8px rgba(39, 99, 49, 0.5);'
       : '0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)'};
   flex-direction: column;
   position: relative;
