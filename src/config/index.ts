19a05da9f43d1e5a883ba31e880a0647273d13ab
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-25 12:26:39 -0400

    Fix PLANT_PER_BLOCK = 0.02

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ BigNumber.config({
 })
 
 export const BSC_BLOCK_TIME = 3
-export const PLANT_PER_BLOCK = new BigNumber(40)
+export const PLANT_PER_BLOCK = new BigNumber(0.02)
 export const BLOCKS_PER_YEAR = new BigNumber((60 / BSC_BLOCK_TIME) * 60 * 24 * 365) // 10512000
 export const BASE_URL = 'https://plantswap.finance'
 export const BASE_EXCHANGE_URL = 'https://exchange.pancakeswap.finance'
@@ -14,3 +14,4 @@ export const BASE_ADD_LIQUIDITY_URL = `${BASE_EXCHANGE_URL}/#/add`
 export const BASE_LIQUIDITY_POOL_URL = `${BASE_EXCHANGE_URL}/#/pool`
 export const LOTTERY_MAX_NUMBER_OF_TICKETS = 50
 export const LOTTERY_TICKET_PRICE = 1
+2
