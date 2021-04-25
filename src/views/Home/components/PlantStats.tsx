8ab514bdf61eccc9085ec180172af80c8e8edd23
PlantSwap.finance <plantswapfinance@gmail.com>
2021-04-25 12:27:54 -0400

    Add PlantStats.tsx

---
+++ src/views/Home/components/PlantStats.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Card, CardBody, Heading, Text } from '@plantswap-libs/uikit'
+import styled from 'styled-components'
+import { getBalanceNumber } from 'utils/formatBalance'
+import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
+import useI18n from 'hooks/useI18n'
+import { getPlantAddress } from 'utils/addressHelpers'
+import CardValue from './CardValue'
+
+const StyledPlantStats = styled(Card)`
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const Row = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 14px;
+  justify-content: space-between;
+  margin-bottom: 8px;
+`
+
+const PlantStats = () => {
+  const TranslateString = useI18n()
+  const totalSupply = useTotalSupply()
+  const burnedBalance = getBalanceNumber(useBurnedBalance(getPlantAddress()))
+  const plantSupply = totalSupply ? getBalanceNumber(totalSupply) - burnedBalance : 0
+
+  return (
+    <StyledPlantStats>
+      <CardBody>
+        <Heading size="xl" mb="24px">
+          {TranslateString(534, 'Plant Stats')}
+        </Heading>
+        <Row>
+          <Text fontSize="14px">{TranslateString(536, 'Total PLANT Supply')}</Text>
+          {plantSupply && <CardValue fontSize="14px" value={plantSupply} />}
+        </Row>
+        <Row>
+          <Text fontSize="14px">{TranslateString(538, 'Total PLANT Burned')}</Text>
+          <CardValue fontSize="14px" decimals={0} value={burnedBalance} />
+        </Row>
+        <Row>
+          <Text fontSize="14px">{TranslateString(540, 'New PLANT/block')}</Text>
+          <CardValue fontSize="14px" decimals={0} value={22} />
+        </Row>
+      </CardBody>
+    </StyledPlantStats>
+  )
+}
+
+export default PlantStats
