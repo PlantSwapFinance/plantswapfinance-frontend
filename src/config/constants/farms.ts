502e7cfd1393ecdec0234de025cca13e057a2307
PlantSwap.finance <plantswapfinance@gmail.com>
2021-05-04 23:44:13 -0400

    link tokens.plant

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -7,10 +7,7 @@ const farms: FarmConfig[] = [
     pid: 0,
     risk: 5,
     lpSymbol: 'PLANT',
-    lpAddresses: {
-      97: '0xFe5Ab583d91fa90549aC61666CF1C4e2CeA5187e',
-      56: '0x58BA5Bd8872ec18BD360a9592149daed2fC57c69',
-    },
+    lpAddresses: tokens.plant.address,
     isTokenOnly: true,
     token: tokens.plant,
     quoteToken: tokens.plant,
