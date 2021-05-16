adba390e29125cb43ad203efa4f7a1a8aac0cce3
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-05-16 18:08:00 -0400

    Add news

--- src/views/Home/components/NewsCard.tsx
+++ src/views/Home/components/NewsCard.tsx
@@ -29,6 +29,8 @@ const NewsCard = () => {
           {TranslateString(542, 'News & Announcements')}
         </Heading>
         <Block>
+          <Label><a href="https://plantswapfinance.medium.com/the-barn-beta-and-we-add-new-farms-7b59d50f2655">The Barn beta and we add new farms</a></Label>
+          <br />
           <Label><a href="https://plantswapfinance.medium.com/its-time-for-gardenv2-91c3241fd226">It’s time for GardenV2</a></Label>
           <br />
           <Label><a href="https://plantswapfinance.medium.com/opening-of-the-garden-new-plant-look-and-more-b8b9ff554e16">Opening of the Garden, new PLANT look, and more!</a></Label>
