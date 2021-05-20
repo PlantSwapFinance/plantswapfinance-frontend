2582809929d7c494641f104bf5097e568650c9e9
PlantSwap.finance <81477245+PlantSwapFinance@users.noreply.github.com>
2021-05-20 17:59:23 -0400

    Add compund btn for PLANT garden

--- src/views/Gardens/components/GardenCard/HarvestAction.tsx
+++ src/views/Gardens/components/GardenCard/HarvestAction.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import BigNumber from 'bignumber.js'
+import styled from 'styled-components'
 import { Button, Flex, Heading, useModal } from '@plantswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
 import { useHarvest } from 'hooks/useHarvest'
+import useStake from 'hooks/useStake'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { usePricePlantBusd } from 'state/hooks'
 import { useWeb3React } from '@web3-react/core'
@@ -14,12 +16,22 @@ interface FarmCardActionsProps {
   lpSymbol?: string
 }
 
+const CompoundAndHarvest = styled.div`
+  padding: 0px
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+`
+
 const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, pid, lpSymbol }) => {
   const { account } = useWeb3React()
   const TranslateString = useI18n()
   const plantPrice = usePricePlantBusd()
   const [pendingTx, setPendingTx] = useState(false)
   const { onReward } = useHarvest(pid)
+  const pidPlant = 0
+  const { onStake } = useStake(pidPlant)
   let earningsBusd = 0
 
   const rawEarningsBalance = account ? getBalanceNumber(earnings) : 0
@@ -31,17 +43,35 @@ const HarvestAction: React.FC<FarmCardActionsProps> = ({ earnings, pid, lpSymbol
   return (
     <Flex mb="8px" justifyContent="space-between" alignItems="center">
       <Heading color={rawEarningsBalance === 0 ? 'textDisabled' : 'text'}>{displayBalance}</Heading>
-      <Button
-        disabled={rawEarningsBalance === 0 || pendingTx}
-        onClick={async () => {
-          setPendingTx(true)
-          await onReward()
-          setPendingTx(false)
-          onHarvestDone()
-        }}
-      >
-        {TranslateString(562, 'Harvest')}
-      </Button>
+      <CompoundAndHarvest>
+        {pid === 0 ? (
+        <Button
+            disabled={rawEarningsBalance === 0 || pendingTx}
+            size="sm"
+            variant="secondary"
+            marginBottom="6px"
+            onClick={async () => {
+              setPendingTx(true)
+              await onStake(rawEarningsBalance.toString())
+              setPendingTx(false)
+            }}
+            >
+              {TranslateString(999, 'Compound')}
+            </Button>
+          ) : null}
+
+        <Button
+          disabled={rawEarningsBalance === 0 || pendingTx}
+          onClick={async () => {
+            setPendingTx(true)
+            await onReward()
+            setPendingTx(false)
+            onHarvestDone()
+          }}
+        >
+          {TranslateString(562, 'Harvest')}
+        </Button>
+      </CompoundAndHarvest>
     </Flex>
   )
 }
--- src/views/Gardens/components/GardenTable/Actions/HarvestAction.tsx
+++ src/views/Gardens/components/GardenTable/Actions/HarvestAction.tsx
@@ -2,15 +2,25 @@ import React, { useState, useRef, useEffect } from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { Button, useModal } from '@plantswap-libs/uikit'
 import BigNumber from 'bignumber.js'
+import styled from 'styled-components'
 import { FarmWithStakedValue } from 'views/Gardens/components/GardenCard/GardenCard'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { useHarvest } from 'hooks/useHarvest'
+import useStake from 'hooks/useStake'
 import useI18n from 'hooks/useI18n'
 import { usePricePlantBusd } from 'state/hooks'
 import { useCountUp } from 'react-countup'
 import ShareModal from 'views/Gardens/components/ShareModal'
 
 import { ActionContainer, ActionTitles, Title, Subtle, ActionContent, Earned, Staked } from './styles'
+
+const CompoundAndHarvest = styled.div`
+  padding: 0px
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+`
 
 const HarvestAction: React.FunctionComponent<FarmWithStakedValue> = ({ pid, userData, token, lpSymbol }) => {
   const { account } = useWeb3React()
@@ -28,6 +38,8 @@ const HarvestAction: React.FunctionComponent<FarmWithStakedValue> = ({ pid, user
 
   const [pendingTx, setPendingTx] = useState(false)
   const { onReward } = useHarvest(pid)
+  const pidPlant = 0
+  const { onStake } = useStake(pidPlant)
   const [onHarvestDone] = useModal(<ShareModal harvested={displayBalance} tokenHarvested={token.symbol} tokenName={lpSymbol} usdHarvested={earningsBusd} />)
   const TranslateString = useI18n()
 
@@ -55,18 +67,36 @@ const HarvestAction: React.FunctionComponent<FarmWithStakedValue> = ({ pid, user
           <Earned>{displayBalance}</Earned>
           <Staked>~{countUp}USD</Staked>
         </div>
-        <Button
-          disabled={!earnings || pendingTx || !account}
-          onClick={async () => {
-            setPendingTx(true)
-            await onReward()
-            setPendingTx(false)
-            onHarvestDone()
-          }}
-          ml="4px"
-        >
-          {TranslateString(562, 'Harvest')}
-        </Button>
+        <CompoundAndHarvest>
+          {pid === 0 ? (
+          <Button
+              disabled={earnings === 0 || pendingTx}
+              size="sm"
+              variant="secondary"
+              marginBottom="6px"
+              onClick={async () => {
+                setPendingTx(true)
+                await onStake(earnings.toString())
+                setPendingTx(false)
+              }}
+              >
+                {TranslateString(999, 'Compound')}
+              </Button>
+            ) : null}
+
+          <Button
+            disabled={!earnings || pendingTx || !account}
+            onClick={async () => {
+              setPendingTx(true)
+              await onReward()
+              setPendingTx(false)
+              onHarvestDone()
+            }}
+            ml="4px"
+          >
+            {TranslateString(562, 'Harvest')}
+          </Button>
+        </CompoundAndHarvest>
       </ActionContent>
     </ActionContainer>
   )
